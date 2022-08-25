29fd8bb85a172f8853755cd9be381f285e644854
Astika Luintel <astild2020@gmail.com>
2022-08-25 12:43:37 -0400

    commit tree ea7df408980a2028821a5c20fcbf841d8fff22fc

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let messages = [];
+    for( let i = 0; i<names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+writeCards(messages, "surprise");
+
+
+
+
+function countDown(i){
+    while( i >= 0)
+    {
+        console.log(i);
+        i--;
+    }
+}
+
+
